b6ddc91dcc68ff07365f02cfe44992dee988a09e
MatyasHroch <matyas.hroch01@upol.cz>
2024-02-12 15:34:11 +0100

    just added the right import

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import { processComponent } from "./controllers/component.js";
 import Card from "./components/Card/Card.js";
 import Article from "./components/Article/Article.js";
 
-import { emit } from "./controllers/emitter.js";
+import { emitter } from "./controllers/emitter.js";
 
 // // creates components and stores them in an array
 const components = [];
@@ -19,5 +19,5 @@ for (let i = 0; i < 1; i++) {
 
 // setTimeout(() => {
 //   console.log("Start changing");
-//   emit("someEvent", -10, "Hello from main");
+//   emitter.emit("someEvent", -10, "Hello from main");
 // }, 5000);
